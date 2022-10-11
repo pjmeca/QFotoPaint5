7bb1869e0329dda049127a18918bdc47370862d4
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2022-10-11 21:07:54 +0200

    Initial commit

---
+++ imagenes.cpp
@@ -0,0 +1,552 @@
+//---------------------------------------------------------------------------
+
+#include "imagenes.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <assert.h>
+
+///////////////////////////////////////////////////////////////////
+/////////  VARIABLES GLOBALES                        //////////////
+///////////////////////////////////////////////////////////////////
+
+ventana foto[MAX_VENTANAS];
+
+tipo_herramienta herr_actual= HER_PUNTO;
+
+int radio_pincel= 10;
+
+Scalar color_pincel= CV_RGB(255, 255, 255);
+
+int difum_pincel= 10;
+
+bool preguntar_guardar= true;
+
+static int numpos= 0; // Número actual en el orden de posición de las ventanas
+
+///////////////////////////////////////////////////////////////////
+/////////  FUNCIONES DE MANEJO DE VENTANAS           //////////////
+///////////////////////////////////////////////////////////////////
+
+void callback (int event, int x, int y, int flags, void *_nfoto);
+
+//---------------------------------------------------------------------------
+
+void inic_fotos (void)
+{
+    for (int i= 0; i<MAX_VENTANAS; i++)
+        foto[i].usada= false;
+}
+
+//---------------------------------------------------------------------------
+
+void fin_fotos (void)
+{
+    for (int i= 0; i<MAX_VENTANAS; i++) {
+        if (foto[i].usada) {
+            destroyWindow(foto[i].nombre);
+            foto[i].usada= false;
+        }
+    }
+}
+
+//---------------------------------------------------------------------------
+
+int primera_libre (void)
+{
+    for (int i= 0; i<MAX_VENTANAS; i++)
+        if (!foto[i].usada)
+            return i;
+    return -1;
+}
+
+//---------------------------------------------------------------------------
+
+void escribir_barra_estado (void)
+{
+    int usadas, modificadas;
+    num_fotos(usadas, modificadas);
+    w->setStatusBarText(QString::number(usadas)+" fotos abiertas, "+
+                        QString::number(modificadas)+" modificadas.");
+}
+
+//---------------------------------------------------------------------------
+
+void crear_nueva (int nfoto, int ancho, int alto, Scalar color)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && !foto[nfoto].usada);
+    foto[nfoto].nombre= "nueva-"+to_string(nfoto)+".png";
+    foto[nfoto].nombref= foto[nfoto].nombre;
+    foto[nfoto].img.create(alto, ancho, CV_8UC3);
+    foto[nfoto].img=  color;
+    namedWindow(foto[nfoto].nombre, WINDOW_NO_POPUP+WINDOW_MOVE_RIGHT);
+    foto[nfoto].orden= numpos++;
+    imshow(foto[nfoto].nombre, foto[nfoto].img);
+    foto[nfoto].usada= true;
+    foto[nfoto].modificada= true;
+    foto[nfoto].roi= Rect(0, 0, ancho, alto);
+    setMouseCallback(foto[nfoto].nombre, callback, reinterpret_cast<void*>(nfoto));
+    escribir_barra_estado();
+}
+
+//---------------------------------------------------------------------------
+
+void crear_nueva (int nfoto, Mat img)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && !foto[nfoto].usada && !img.empty());
+    foto[nfoto].nombre= "nueva-"+to_string(nfoto)+".png";
+    foto[nfoto].nombref= foto[nfoto].nombre;
+    foto[nfoto].img= img;
+    namedWindow(foto[nfoto].nombre, WINDOW_NO_POPUP+WINDOW_MOVE_RIGHT);
+    foto[nfoto].orden= numpos++;
+    imshow(foto[nfoto].nombre, foto[nfoto].img);
+    foto[nfoto].usada= true;
+    foto[nfoto].modificada= true;
+    foto[nfoto].roi= Rect(0, 0, img.cols, img.rows);
+    setMouseCallback(foto[nfoto].nombre, callback, reinterpret_cast<void*>(nfoto));
+    escribir_barra_estado();
+}
+
+//---------------------------------------------------------------------------
+
+void crear_nueva (int nfoto, string nombre)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && !foto[nfoto].usada);
+    foto[nfoto].nombre= nombre;
+    foto[nfoto].nombref= Lt1(nombre);
+    foto[nfoto].img= imread(foto[nfoto].nombref);
+    if (foto[nfoto].img.empty())
+        return;
+    namedWindow(foto[nfoto].nombre, WINDOW_NO_POPUP+WINDOW_MOVE_RIGHT);
+    foto[nfoto].orden= numpos++;
+    imshow(foto[nfoto].nombre, foto[nfoto].img);
+    foto[nfoto].usada= true;
+    foto[nfoto].modificada= false;
+    foto[nfoto].roi= Rect(0, 0, foto[nfoto].img.cols, foto[nfoto].img.rows);
+    setMouseCallback(foto[nfoto].nombre, callback, reinterpret_cast<void*>(nfoto));
+    escribir_barra_estado();
+}
+
+//---------------------------------------------------------------------------
+
+void guardar_foto (int nfoto, string nombre)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && foto[nfoto].usada);
+    foto[nfoto].modificada= false;
+    // 1. Guardar la imagen con el mismo nombre que tiene
+    if (nombre=="")
+        imwrite(foto[nfoto].nombref, foto[nfoto].img);
+    // 2. Guardar la imagen con un nombre distinto al actual
+    else {
+        imwrite(Lt1(nombre), foto[nfoto].img);
+        setWindowTitle(foto[nfoto].nombre, nombre);
+    }
+    escribir_barra_estado();
+}
+
+//---------------------------------------------------------------------------
+
+void cerrar_foto (int nfoto, bool destruir_ventana)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && foto[nfoto].usada);
+    if (destruir_ventana)
+        destroyWindow(foto[nfoto].nombre);
+    foto[nfoto].usada= false;
+    escribir_barra_estado();
+}
+
+//---------------------------------------------------------------------------
+
+void mostrar (int nfoto)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && foto[nfoto].usada);
+    imshow(foto[nfoto].nombre, foto[nfoto].img);
+    escribir_barra_estado();
+}
+
+//---------------------------------------------------------------------------
+
+int nombre_a_numero (string nombre)
+{
+    for (int i= 0; i<MAX_VENTANAS; i++)
+        if (foto[i].usada && foto[i].nombre==nombre)
+            return i;
+    return -1;
+}
+
+//---------------------------------------------------------------------------
+
+int foto_activa (void)
+{
+    int maxorden= -1, maxpos= -1;
+    for (int i= 0; i<MAX_VENTANAS; i++) {
+        if (foto[i].usada && foto[i].orden>maxorden) {
+            maxorden= foto[i].orden;
+            maxpos= i;
+        }
+    }
+    return maxpos;
+}
+
+//---------------------------------------------------------------------------
+
+int num_fotos (int &usadas, int &modificadas)
+{
+    usadas= 0;
+    modificadas= 0;
+    for (int i= 0; i<MAX_VENTANAS; i++) {
+        if (foto[i].usada) {
+            usadas++;
+            if (foto[i].modificada)
+                modificadas++;
+        }
+    }
+    return usadas;
+}
+
+//---------------------------------------------------------------------------
+
+void set_callback_foto (int nfoto, bool activo)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && foto[nfoto].usada);
+    if (!activo)
+        setMouseCallback(foto[nfoto].nombre, nullptr);
+    else
+        setMouseCallback(foto[nfoto].nombre, callback, reinterpret_cast<void*>(nfoto));
+}
+
+///////////////////////////////////////////////////////////////////
+/////////  FUNCIONES DEL CALLBACK PRINCIPAL          //////////////
+///////////////////////////////////////////////////////////////////
+
+static int downx, downy;
+// Posición inicial del ratón al pinchar sobre la imagen actual
+
+//---------------------------------------------------------------------------
+
+void ninguna_accion (int factual, int x, int y)
+{
+    Mat res= foto[factual].img.clone();
+    circle(res, Point(x, y), radio_pincel, CV_RGB(255,255,255), 2, LINE_AA);
+    imshow(foto[factual].nombre, res);
+}
+
+//---------------------------------------------------------------------------
+
+void cb_close (int factual)
+{
+    if (foto[factual].usada && !foto[factual].img.empty()) {
+        if (foto[factual].modificada && preguntar_guardar) {
+            QString cadena= "El archivo " + QString::fromStdString(foto[factual].nombre) +
+                    " ha sido modificado.\n¿Desea guardarlo?";
+            int resp= QMessageBox::question(w, "Archivo modificado", cadena,
+                                            QMessageBox::Yes | QMessageBox::No );
+            if (resp==QMessageBox::Yes)
+                guardar_foto(factual);
+        }
+        cerrar_foto(factual, false);
+    }
+}
+
+//---------------------------------------------------------------------------
+
+void cb_punto (int factual, int x, int y)
+{
+    Mat im= foto[factual].img;  // Ojo: esto no es una copia, sino a la misma imagen
+    if (difum_pincel==0)
+        circle(im, Point(x, y), radio_pincel, color_pincel, -1, LINE_AA);
+    else {
+        Mat res(im.size(), im.type(), color_pincel);
+        Mat cop(im.size(), im.type(), CV_RGB(0,0,0));
+        circle(cop, Point(x, y), radio_pincel, CV_RGB(255,255,255), -1, LINE_AA);
+        blur(cop, cop, Size(difum_pincel*2+1, difum_pincel*2+1));
+        multiply(res, cop, res, 1.0/255.0);
+        bitwise_not(cop, cop);
+        multiply(im, cop, im, 1.0/255.0);
+        im= res + im;
+    }
+    imshow(foto[factual].nombre, im);
+    foto[factual].modificada= true;
+}
+
+//---------------------------------------------------------------------------
+
+void cb_linea (int factual, int x, int y)
+{
+    Mat im= foto[factual].img;  // Ojo: esto no es una copia, sino a la misma imagen
+    if (difum_pincel==0)
+        line(im, Point(downx, downy), Point(x,y), color_pincel, radio_pincel*2+1);
+    else {
+        Mat res(im.size(), im.type(), color_pincel);
+        Mat cop(im.size(), im.type(), CV_RGB(0,0,0));
+        line(cop, Point(downx, downy), Point(x,y), CV_RGB(255,255,255), radio_pincel*2+1);
+        blur(cop, cop, Size(difum_pincel*2+1, difum_pincel*2+1));
+        multiply(res, cop, res, 1.0/255.0);
+        bitwise_not(cop, cop);
+        multiply(im, cop, im, 1.0/255.0);
+        im= res + im;
+    }
+    imshow(foto[factual].nombre, im);
+    foto[factual].modificada= true;
+}
+
+//---------------------------------------------------------------------------
+
+void cb_ver_linea (int factual, int x, int y)
+{
+    Mat res= foto[factual].img.clone();
+    line(res, Point(downx, downy), Point(x,y), color_pincel, radio_pincel*2+1);
+    imshow(foto[factual].nombre, res);
+}
+
+//---------------------------------------------------------------------------
+
+void cb_rectangulo (int factual, int x, int y)
+{
+    Mat im= foto[factual].img;
+    if (difum_pincel==0)
+        rectangle(im, Point(downx, downy), Point(x,y), color_pincel, radio_pincel*2-1);
+    else {
+        Mat res(im.size(), im.type(), color_pincel);
+        Mat cop(im.size(), im.type(), CV_RGB(0,0,0));
+        rectangle(cop, Point(downx, downy), Point(x,y), CV_RGB(255,255,255), radio_pincel*2-1);
+        blur(cop, cop, Size(difum_pincel*2+1, difum_pincel*2+1));
+        multiply(res, cop, res, 1.0/255.0);
+        bitwise_not(cop, cop);
+        multiply(im, cop, im, 1.0/255.0);
+        im= res + im;
+    }
+    imshow(foto[factual].nombre, im);
+    foto[factual].modificada= true;
+}
+
+//---------------------------------------------------------------------------
+
+void cb_ver_rectangulo(int factual, int x, int y)
+{
+    Mat res= foto[factual].img.clone();
+    rectangle(res, Point(downx, downy), Point(x,y), color_pincel, radio_pincel*2-1); // negativo para que rellene el rectangulo si el radio del pincel es 0 (si rectangle recibe un número negativo, lo rellena)
+    imshow(foto[factual].nombre, res);
+}
+
+//---------------------------------------------------------------------------
+
+void cb_seleccionar (int factual, int x, int y)
+{
+    Mat im= foto[factual].img;
+    Rect nuevo= Rect(min(downx, x), min(downy, y),
+                     max(downx, x)-min(downx, x)+1, max(downy, y)-min(downy, y)+1);
+    if (nuevo.x<0)
+        nuevo.x= 0;
+    if (nuevo.y<0)
+        nuevo.y= 0;
+    if (nuevo.x+nuevo.width>im.size().width)
+        nuevo.width= im.size().width-nuevo.x;
+    if (nuevo.y+nuevo.height>im.size().height)
+        nuevo.height= im.size().height-nuevo.y;
+    foto[factual].roi= nuevo;
+}
+
+//---------------------------------------------------------------------------
+
+void cb_ver_seleccion (int factual, int x, int y, bool foto_roi)
+{
+    Mat res= foto[factual].img.clone();
+    Point p1, p2;
+    if (foto_roi) {
+        p1.x= foto[factual].roi.x;
+        p1.y= foto[factual].roi.y;
+        p2.x= foto[factual].roi.x+foto[factual].roi.width-1;
+        p2.y= foto[factual].roi.y+foto[factual].roi.height-1;
+    }
+    else {
+        p1= Point(downx, downy);
+        p2= Point(x, y);
+    }
+    rectangle(res, p1, p2, CV_RGB(255,foto_roi?0:255,0),2);
+    imshow(foto[factual].nombre, res);
+}
+
+//---------------------------------------------------------------------------
+
+void callback (int event, int x, int y, int flags, void *_nfoto)
+{
+    int factual= reinterpret_cast<int>(_nfoto);
+
+    // 1. Eventos y casos especiales
+    // 1.1. Evento cerrar ventana
+    if (event==EVENT_CLOSE) {
+        cb_close(factual);
+        return;
+    }
+    // 1.2. Evento obtiene el foco
+    if (event==EVENT_FOCUS) {
+        foto[factual].orden= numpos++;
+    }
+    // 1.3. El ratón se sale de la ventana
+    if (x<0 || x>=foto[factual].img.size().width || y<0 || y>=foto[factual].img.size().height)
+        return;
+    // 1.4. Se inicia la pulsación del ratón
+    if (event==EVENT_LBUTTONDOWN) {
+        downx= x;
+        downy= y;
+    }
+
+    // 2. Según la herramienta actual
+    switch (herr_actual) {
+
+    // 2.1. Herramienta PUNTO
+    case HER_PUNTO:
+        if (flags==EVENT_FLAG_LBUTTON)
+            cb_punto(factual, x, y);
+        else
+            ninguna_accion(factual, x, y);
+        break;
+
+        // 2.2. Herramienta LINEA
+    case HER_LINEA:
+        if (event==EVENT_LBUTTONUP)
+            cb_linea(factual, x, y);
+        else if (event==EVENT_MOUSEMOVE && flags==EVENT_FLAG_LBUTTON)
+            cb_ver_linea(factual, x, y);
+        else
+            ninguna_accion(factual, x, y);
+        break;
+
+        // 2.3. Herramienta RECTANGULO
+    case HER_RECTANGULO:
+        if (event==EVENT_LBUTTONUP)
+            cb_rectangulo(factual, x, y);
+        else if (event==EVENT_MOUSEMOVE && flags==EVENT_FLAG_LBUTTON)
+            cb_ver_rectangulo(factual, x, y);
+        else
+            ninguna_accion(factual, x, y);
+        break;
+
+        // 2.4. Herramienta SELECCION
+    case HER_SELECCION:
+        if (event==EVENT_LBUTTONUP)
+            cb_seleccionar(factual, x, y);
+        else if (event==EVENT_MOUSEMOVE)
+            cb_ver_seleccion(factual, x, y, flags!=EVENT_FLAG_LBUTTON);
+        break;
+    }
+    escribir_barra_estado();
+}
+
+
+///////////////////////////////////////////////////////////////////
+/////////  FUNCIONES DE PROCESAMIENTO DE IMAGENES    //////////////
+///////////////////////////////////////////////////////////////////
+
+void invertir (int nfoto, int nres)
+{
+    Mat img(foto[nfoto].img.size(), foto[nfoto].img.type());
+    bitwise_not(foto[nfoto].img, img);
+    crear_nueva(nres, img);
+}
+
+//---------------------------------------------------------------------------
+
+void rotar_angulo (Mat imagen, Mat &imgRes, double angulo, double escala, int modo)
+{
+    double w= imagen.size().width, h= imagen.size().height;
+    Size sres;
+    if (modo==0) {     // Reescalar con proporción al original
+        sres.width= int(w*escala);
+        sres.height= int(h*escala);
+    }
+    else if (modo==1)  // Reescalar y ampliar para caber entera
+        sres.width= sres.height= int(sqrt(w*w + h*h)*escala);
+    else               // Reescalar y recortar para no mostrar borde
+        sres.width= sres.height= int(min(w, h)*escala/sqrt(2.0));
+    imgRes.create(sres, imagen.type());
+    double sa= sin(angulo*M_PI/180), ca= cos(angulo*M_PI/180);
+    double cx= -w/2*ca-h/2*sa, cy= w/2*sa-h/2*ca;
+    Mat M= getRotationMatrix2D(Point2f(0,0), angulo, escala);
+    M.at<double>(0,2)= sres.width/2+cx*escala;
+    M.at<double>(1,2)= sres.height/2+cy*escala;
+    imgRes= color_pincel;
+    warpAffine(imagen, imgRes, M, sres, INTER_CUBIC);
+}
+
+//---------------------------------------------------------------------------
+
+void rotar_exacto (int nfoto, int nres, int grado)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && foto[nfoto].usada);
+    assert(nres>=0 && nres<MAX_VENTANAS && !foto[nres].usada);
+    Mat entrada= foto[nfoto].img;
+    Mat salida;
+    if (grado == 0)
+        salida= entrada.clone();
+    else if (grado == 1) {
+        transpose(entrada, salida);
+        flip(salida, salida, 1);
+    }
+    else if (grado == 2)
+        flip(entrada, salida, -1);
+    else if (grado == 3) {
+        transpose(entrada, salida);
+        flip(salida, salida, 0);
+    }
+    crear_nueva(nres, salida);
+}
+
+//---------------------------------------------------------------------------
+
+void ver_brillo_contraste (int nfoto, double suma, double prod, bool guardar)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && foto[nfoto].usada);
+    Mat img;
+    foto[nfoto].img.convertTo(img, CV_8UC3, prod, suma);
+    imshow(foto[nfoto].nombre, img);
+    if (guardar) {
+        img.copyTo(foto[nfoto].img);
+        foto[nfoto].modificada= true;
+    }
+}
+
+//---------------------------------------------------------------------------
+
+void ver_suavizado (int nfoto, int ntipo, int tamx, int tamy, bool guardar)
+{
+    assert(nfoto>=0 && nfoto<MAX_VENTANAS && foto[nfoto].usada);
+    assert(tamx>0 && tamx&1 && tamy>0 && tamy&1);
+    Mat img= foto[nfoto].img.clone();
+    if (ntipo == 1)
+        GaussianBlur(foto[nfoto].img, img, Size(tamx, tamy), 0);
+    else if (ntipo == 2)
+        blur(foto[nfoto].img, img, Size(tamx, tamy));
+    imshow(foto[nfoto].nombre, img);
+    if (guardar) {
+        img.copyTo(foto[nfoto].img);
+        foto[nfoto].modificada= true;
+    }
+}
+
+//---------------------------------------------------------------------------
+
+void media_ponderada (int nf1, int nf2, int nueva, double peso)
+{
+    assert(nf1>=0 && nf1<MAX_VENTANAS && foto[nf1].usada);
+    assert(nf2>=0 && nf2<MAX_VENTANAS && foto[nf2].usada);
+    assert(nueva>=0 && nueva<MAX_VENTANAS && !foto[nueva].usada);
+    Mat img= foto[nf1].img.clone();
+    Mat cop;
+    resize(foto[nf2].img, cop, img.size());
+    addWeighted(img, peso, cop, 1.0-peso, 0, img);
+    crear_nueva(nueva, img);
+}
+
+//---------------------------------------------------------------------------
+
+string Lt1(string cadena)
+{
+    QString temp= QString::fromUtf8(cadena.c_str());
+    return temp.toLatin1().data();
+}
+
+//---------------------------------------------------------------------------
